64910ded9d426d173cb8efe5e10879a58c0e0c69
Nozomi Tanetani <nozomi@wifi-139-172.sfc.wide.ad.jp>
2019-11-14 01:18:22 +0900

    commit

--- frqi_real.py
+++ frqi_real.py
@@ -4,17 +4,8 @@ from qiskit.qasm import pi
 from qiskit.tools.visualization import plot_histogram, circuit_drawer
 import numpy as np
 
-token = "e44ac171fe282a3c0d690386deb91d57028dcd020f13563bb33e0fa861bc3278c0b5ac6df23a4588aa723676ce76af1646f9e0118b2a497c5965fd43d8669336"
-url = "https://q-console-api.mybluemix.net/api/Hubs/ibm-q-keio/Groups/keio-internal/Projects/keio-students"
-
-IBMQ.enable_account(token, url)
-# syntax and names are different from these two
-# at the moment, only IBMQ shows real machine
-backends = IBMQ.backends()
-# backends = IBMQ.backends(simulator=False)
-print("IBMQ backends:",backends)
-# backends = Aer.backends()
-# print("Aer backends:",backends)
+IBMQ.load_account()
+provider = IBMQ.get_provider(hub='ibm-q-keio', group='keio-internal', project='keio-students')
 
 q = QuantumRegister(3)
 c = ClassicalRegister(3)
@@ -27,32 +18,32 @@ def rry(circ, con1, con2, angle, t, c0, c1):
 
     circ.rz(angle/2, t)
 
-    if(con1 == 1):
+    if(con1 == 0):
         circ.x(c0)
 
     circ.cx(c0, t)
 
-    if(con1 == 1):
+    if(con1 == 0):
         circ.x(c0)
 
     circ.rz(-angle/2, t)
 
-    if(con2 == 1):
+    if(con2 == 0):
         circ.x(c1)
 
     circ.cx(c1, t)
 
-    if(con2 == 1):
+    if(con2 == 0):
         circ.x(c1)
 
     circ.rz(angle/2, t)
 
-    if(con1 == 1):
+    if(con1 == 0):
         circ.x(c0)
 
     circ.cx(c0, t)
 
-    if(con1 == 1):
+    if(con1 == 0):
         circ.x(c0)
 
     circ.rz(-angle/2, t)
@@ -73,10 +64,7 @@ for i in range(len(q)):
 # backends = ['ibmq_20_tokyo', 'ibmq_qasm_simulator']
 
 # Use this for the real machine
-backend_sim = IBMQ.get_backend('ibmq_singapore')
-# Use this for the simulation
-#backend_sim = IBMQ.get_backend('ibmq_qasm_simulator')
-
+backend_sim = provider.get_backend('ibmq_singapore')
 
 result = execute(qc, backend_sim, shots=4096).result()
 #circuit_drawer(qc).show()
