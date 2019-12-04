471721c912aedbee61fd51822af39cf91878518f
ntanetani <n.tanetani@gmail.com>
2019-12-04 21:47:13 +0900

    fix the control

--- frqi_real.py
+++ frqi_real.py
@@ -17,47 +17,47 @@ def rry(circ, con1, con2, angle, t, c0, c1):
 
     circ.h(t)
 
-    circ.rz(angle/2, t)
+    circ.rz(angle/4, t)
 
-    if(con1 == 0):
+    if(con2 == 0):
         circ.x(c0)
 
     circ.cx(c0, t)
 
-    if(con1 == 0):
+    if(con2 == 0):
         circ.x(c0)
 
-    circ.rz(-angle/2, t)
+    circ.rz(-angle/4, t)
 
-    if(con2 == 0):
+    if(con1 == 0):
         circ.x(c1)
 
     circ.cx(c1, t)
 
-    if(con2 == 0):
+    if(con1 == 0):
         circ.x(c1)
 
-    circ.rz(angle/2, t)
+    circ.rz(angle/4, t)
 
-    if(con1 == 0):
+    if(con2 == 0):
         circ.x(c0)
 
     circ.cx(c0, t)
 
-    if(con1 == 0):
+    if(con2 == 0):
         circ.x(c0)
 
-    circ.rz(-angle/2, t)
+    circ.rz(-angle/4, t)
 
     circ.h(t)
 
 for i in range(1,len(q)):
     qc.h(q[i])
 
-rry(qc, 0, 0, np.pi/2, q[0], q[1], q[2])
-rry(qc, 0, 1, np.pi/4, q[0], q[1], q[2])
-rry(qc, 1, 0, np.pi/6, q[0], q[1], q[2])
-rry(qc, 1, 1, np.pi/8, q[0], q[1], q[2])
+rry(qc, 0, 0, 2 * np.pi/2, q[0], q[1], q[2])
+rry(qc, 0, 1, 2 * np.pi/4, q[0], q[1], q[2])
+rry(qc, 1, 0, 2 * np.pi/6, q[0], q[1], q[2])
+rry(qc, 1, 1, 2 * np.pi/8, q[0], q[1], q[2])
 
 for i in range(len(q)):
     qc.measure(q[i],c[i])
@@ -66,6 +66,9 @@ for i in range(len(q)):
 
 # Use this for the real machine
 backend_sim = provider.get_backend('ibmq_london')
+
+# Use this for the simulator
+#backend_sim = provider.get_backend('ibmq_qasm_simulator')
 
 result = execute(qc, backend_sim, shots=4096).result()
 #circuit_drawer(qc).show()
